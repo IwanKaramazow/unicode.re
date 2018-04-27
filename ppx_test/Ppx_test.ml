9355e4669f7b075938f9fc58d4038ba84a7b150a
Iwan <maxim@piesync.com>
2018-04-27 19:26:50 +0200

    Update bsb-native & use new ppx feature

---
+++ ppx_test/Ppx_test.ml
@@ -0,0 +1,8 @@
+(* let u = Uchar.fromInt 97 *)
+
+(* let () = match%uchar u with *)
+  (* | 'a' -> print_endline "this works" *)
+  (* | 1 -> print_endline "compile" *)
+  (* | n when n < 10 -> print_endline "ok" *)
+  (* | _ -> print_endline "catch all" *)
+
